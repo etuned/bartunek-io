040b3036467a82af5f5b90a7a96b31afae042137
Edwin Bartunek <edwin@devhart.io>
2026-07-11 00:09:11 +0900

    code cleanup

--- src/components/date-formater.tsx
+++ src/components/date-formater.tsx
@@ -1,33 +1,33 @@
-
-import { tz, tzName } from "@date-fns/tz";
-import { format } from "date-fns";
-
+import { tz, tzName } from '@date-fns/tz';
+import { format } from 'date-fns';
 
 function FormatDatetime({
-    dateObject,
-    elementProps }: {
-        dateObject: {
-            datetimeString: string,
-            formatter?: string,
-            timezone?: string | "none"
-        };
-        elementProps?: React.ComponentProps<"span">
-    }) {
-    // if (!window && !dateObject?.timezone) throw new Error("FormatDateTime must define timezone for server. Otherwise use clientOnly and put a Skeleton in fallback.")
+	dateObject,
+	elementProps,
+}: {
+	dateObject: {
+		datetimeString: string;
+		formatter?: string;
+		timezone?: string | 'none';
+	};
+	elementProps?: React.ComponentProps<'span'>;
+}) {
 
-    const date = new Date(dateObject?.datetimeString)
-    const formatter = dateObject?.formatter ? dateObject?.formatter : 'MM/dd/yy hh:mm aaa'
-    const timezone = dateObject?.timezone && dateObject?.timezone !== "none" ? dateObject?.timezone : Intl.DateTimeFormat()?.resolvedOptions()?.timeZone
-    return (
-        <span
-            {...elementProps}
-        >
-            {format(date, formatter, { in: tz(timezone) })}
-            {dateObject?.timezone !== "none" && ' '}
-            {dateObject?.timezone !== "none" && tzName(timezone, date, 'short')}
-
-        </span>
-    )
+	const date = new Date(dateObject?.datetimeString);
+	const formatter = dateObject?.formatter
+		? dateObject?.formatter
+		: 'MM/dd/yy hh:mm aaa';
+	const timezone =
+		dateObject?.timezone && dateObject?.timezone !== 'none'
+			? dateObject?.timezone
+			: Intl.DateTimeFormat()?.resolvedOptions()?.timeZone;
+	return (
+		<span {...elementProps}>
+			{format(date, formatter, { in: tz(timezone) })}
+			{dateObject?.timezone !== 'none' && ' '}
+			{dateObject?.timezone !== 'none' && tzName(timezone, date, 'shortGeneric')}
+		</span>
+	);
 }
 
-export { FormatDatetime }
+export { FormatDatetime };
