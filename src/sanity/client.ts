e4073ae189122cb3468b789dd1ecbcb103b84d13
Edwin Bartunek <edwin@devhart.io>
2026-08-23 15:33:19 +0900

    add query options

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -11,3 +11,14 @@ export const client = createClient({
   apiVersion,
   useCdn,
 })
+
+import {createQueryStore} from '@sanity/react-loader'
+
+export const {
+  // Used only server side
+  loadQuery,
+  setServerClient,
+  // Used only client side
+  useQuery,
+  useLiveMode,
+} = createQueryStore({client: false, ssr: true})
